079980027afbd4076595cd41998c3dbd2f8cd3cc
Kan <70838508+kan6868@users.noreply.github.com>
2025-06-19 21:45:09 +0700

    Update Rtt_EmscriptenContext.cpp

--- platform/emscripten/Rtt_EmscriptenContext.cpp
+++ platform/emscripten/Rtt_EmscriptenContext.cpp
@@ -540,11 +540,11 @@ namespace Rtt
 		}
 
 
-		Uint32 flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE;
+		Uint32 flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
 		//flags |= (fMode == "fullscreen") ?  SDL_WINDOW_FULLSCREEN_DESKTOP : SDL_WINDOW_RESIZABLE;
 		//Window canvas
-				// const pixelRatio = jsContextGetPixelRatio();
-		fWindow = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, fWidth, fHeight, flags);
+		const int pixelRatio = jsContextGetPixelRatio();
+		fWindow = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, fWidth * pixelRatio, fHeight * pixelRatio, flags);
 		SDL_GL_CreateContext(fWindow);
 		fPlatform->setWindow(fWindow, fOrientation);
 
