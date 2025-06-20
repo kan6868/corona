1d94f79e32ea07e7f84441e92e080aa58d092bad
Kan <70838508+kan6868@users.noreply.github.com>
2025-06-20 07:18:07 +0700

    Update Rtt_EmscriptenContext.cpp

--- platform/emscripten/Rtt_EmscriptenContext.cpp
+++ platform/emscripten/Rtt_EmscriptenContext.cpp
@@ -550,7 +550,7 @@ namespace Rtt
 
 		fWindow = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, fWidth, fHeight, flags);
 		SDL_GL_CreateContext(fWindow);
-		SDL_SetWindowPosition(fWindow, fWidth * (pixelRatio * .5), fHeight * (pixelRatio * .5))
+		SDL_SetWindowPosition(fWindow, fWidth * (pixelRatio * .5), fHeight * (pixelRatio * .5));
 		fPlatform->setWindow(fWindow, fOrientation);
 
 #if defined(EMSCRIPTEN)
