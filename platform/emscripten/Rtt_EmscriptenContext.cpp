f725735b66437fa17a2ec0b7bb79e517f2fe36a5
Kan <70838508+kan6868@users.noreply.github.com>
2025-10-14 12:32:00 +0700

    Update Rtt_EmscriptenContext.cpp

--- platform/emscripten/Rtt_EmscriptenContext.cpp
+++ platform/emscripten/Rtt_EmscriptenContext.cpp
@@ -396,7 +396,7 @@ namespace Rtt
 			Rtt_LogException("SDL_Init: %s(%d)\n", SDL_GetError(), rc);
 		}
 
-		SDL_SetHint(SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH, "1");
+		// SDL_SetHint(SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH, "1");
 
 		SDL_version compiled;
 		SDL_version linked;
