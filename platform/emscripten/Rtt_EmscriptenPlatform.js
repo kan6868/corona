b61c81d7c84fd7ec245e11ce799124195d91964a
Kan <70838508+kan6868@users.noreply.github.com>
2025-10-16 11:01:31 +0700

    Update Rtt_EmscriptenPlatform.js

--- platform/emscripten/Rtt_EmscriptenPlatform.js
+++ platform/emscripten/Rtt_EmscriptenPlatform.js
@@ -600,8 +600,8 @@ var platformLibrary =
 		Module.appContentHeight = h;
 	},
 
-	jsContextGetWindowWidth: function () { return window.innerWidth; },
-	jsContextGetWindowHeight: function () { return window.innerHeight; },
+	jsContextGetWindowWidth: function () { return window.innerWidth * window.devicePixelRatio; },
+	jsContextGetWindowHeight: function () { return window.innerHeight * window.devicePixelRatio; },
 
 	jsContextUnlockAudio: function () {
 		// create empty buffer and play it
